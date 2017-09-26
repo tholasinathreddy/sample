c9d42e9b92d4a53784dd918d87bf2fc189e2b672
tholasinath999 <tholasinath999@gmail.com>
2017-09-26 19:02:06 +0530

    Adding Sample Controller

---
+++ app/Http/Controllers/SampleController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SampleController extends Controller
+{
+    // Sample Controller Change done using branch
+	// Sample comment added using SamplecontrollerBanch
+}
